035d6837c86a528085e3d8078f6c810606974067
Vinicius <vinicius.gabriel83@yahoo.com>
2022-02-16 08:33:35 -0300

    Add project files.

---
+++ ChessProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChessProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
